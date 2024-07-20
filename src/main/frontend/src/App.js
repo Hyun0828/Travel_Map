44351967593094819c2c4113d65adca4999da126
Hyun0828 <spdlqj4818@naver.com>
2024-07-21 01:11:35 +0900

    Front를 통한 OAuth2 로그인 구현 완료 -> 아직 JWT 연동 X

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from "./components/LoginPage";
 import KakaoRedirectPage from "./components/KakaoRedirectPage";
+import NaverRedirectPage from "./components/NaverRedirectPage";
+import GoogleRedirectPage from "./components/GoogleRedirectPage";
+
 
 const App = () => {
     return (
@@ -9,6 +12,8 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<LoginPage />}></Route>
                     <Route path="/login/oauth2/code/kakao" element={<KakaoRedirectPage />}></Route>
+                    <Route path="/login/oauth2/code/naver" element={<NaverRedirectPage />}></Route>
+                    <Route path="/login/oauth2/code/google" element={<GoogleRedirectPage/>}></Route>
                 </Routes>
             </BrowserRouter>
         </div>
--- src/main/frontend/src/components/LoginPage.js
+++ src/main/frontend/src/components/LoginPage.js
@@ -1,18 +1,46 @@
 import React from 'react';
 
 const LoginPage = () => {
-    const handleButtonClick = () => {
+    const handleKakaoLoginClick = () => {
         window.location.href = 'http://localhost:8080/oauth/kakao';
     };
 
+    const handleNaverLoginClick = () => {
+        window.location.href = 'http://localhost:8080/oauth/naver';
+    }
+
+    const handleGoogleLoginClick = () => {
+        window.location.href = "http://localhost:8080/oauth/google";
+    }
+
     return (
-        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
-            <button
-                onClick={handleButtonClick}
-                style={{padding: '10px 20px', fontSize: '18px', borderRadius: '5px', cursor: 'pointer'}}
-            >
-                카카오톡 로그인
-            </button>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <div>
+                <button
+                    onClick={handleKakaoLoginClick}
+                    style={{
+                        padding: '10px 20px',
+                        fontSize: '18px',
+                        borderRadius: '5px',
+                        cursor: 'pointer',
+                        marginRight: '10px'
+                    }}
+                >
+                    카카오톡 로그인
+                </button>
+                <button
+                    onClick={handleNaverLoginClick}
+                    style={{padding: '10px 20px', fontSize: '18px', borderRadius: '5px', cursor: 'pointer'}}
+                >
+                    네이버 로그인
+                </button>
+                <button
+                    onClick={handleGoogleLoginClick}
+                    style={{padding: '10px 20px', fontSize: '18px', borderRadius: '5px', cursor: 'pointer'}}
+                >
+                    구글 로그인
+                </button>
+            </div>
         </div>
     );
 };
